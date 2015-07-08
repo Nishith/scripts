6f22374a5301daba0d0c7c9867c94af4218fd074
Nishith <nishith.nand@gmail.com>
2015-07-08 00:18:35 -0700

    Adding scripts

--- install_java.sh
+++ install_java.sh
@@ -1,6 +1,7 @@
 cd /opt/
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u45-b14/jdk-8u45-linux-x64.tar.gz"
 tar xzf jdk-8u45-linux-x64.tar.gz
+rm -f jdk-8u45-linux-x64.tar.gz
 cd /opt/jdk1.8.0_45/
 alternatives --install /usr/bin/java java /opt/jdk1.8.0_45/bin/java 2
 alternatives --install /usr/bin/jar jar /opt/jdk1.8.0_45/bin/jar 2
